d448e83dfbc73be7b5fca4bc1dc07855719adc9e
mmarenkov <marina.marenkov@gmail.com>
2023-09-03 09:40:18 +0300

    adding employee json file

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ app.add_middleware(CORSMiddleware, allow_origins=["https://chat.openai.com"])
 with open("./data/products.json", "r") as f:
     products = json.load(f)
 
+with open("./data/employees.json", "r") as e:
+    employees = json.load(f)
+
 @app.get("/products", summary="Get a list of products", operation_id="getProducts")
 async def get_products(query: str = None):
     """
@@ -21,12 +24,19 @@ async def get_products(query: str = None):
             if all(keyword in str(product.values()).lower() for keyword in keywords)
         ]
     return products
+
 @app.get("/employees", summary="Get a list of employees", operation_id="getEmployees")
 async def get_employees(query: str = None):
-    """
-    Returns a list of employees, optionally filtered by providing a query parameter.
-    """
-    employees = [{
+    
+    # Returns a list of employees, optionally filtered by providing a query parameter.
+    if query:
+        keywords = query.lower().split()
+        return [
+            employee
+            for employee in employees
+            if all(keyword in str(employee.values()).lower() for keyword in keywords)
+        ]
+    """ employees = [{
       "name": "Eli",
       "description": "Most valuable employee",
       "category": "CSA",
@@ -39,5 +49,5 @@ async def get_employees(query: str = None):
       "category": "Free-lancer",
       "size": "36",
       "price": 129.99
-    }]
+    }] """
     return employees
